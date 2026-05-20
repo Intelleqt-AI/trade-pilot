8d16e42b49aab2fa0b59c7b9823d6d966f5909f9
Saif Hasan <saif@intelleqt.ai>
2026-05-20 18:21:00 +0600

    Trade pilot landing page removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Index from "./pages/Index";
 import CustomerDashboard from "./pages/CustomerDashboard";
-import FindTradespeople from "./pages/FindTradespeople";
-import ReviewsReputation from "./pages/ReviewsReputation";
-import Support from "./pages/Support";
 import TradeRegistration from "./pages/TradeRegistration";
 import CustomerRegistration from "./pages/CustomerRegistration";
 import Login from "./pages/Login";
@@ -15,15 +11,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import Profile from "./pages/Profile";
 import VerifyEmail from "./pages/VerifyEmail";
-import Trades from "./pages/Trades";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
 import NotFound from "./pages/NotFound";
-import Plumbers from "./pages/Plumbers";
-import Electricians from "./pages/Electricians";
-import Builders from "./pages/Builders";
-import Roofers from "./pages/Roofers";
-import { DemoModeBanner } from "./components/DemoModeBanner";
 import TradeCRMLayout from "./layouts/TradeCRMLayout";
 import Dashboard from "./pages/trade-crm/Dashboard";
 import JobMarket from "./pages/trade-crm/JobMarket";
@@ -42,16 +30,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* <DemoModeBanner /> */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/dashboard" element={<CustomerDashboard />} />
-          <Route path="/plumbers" element={<Plumbers />} />
-          <Route path="/electricians" element={<Electricians />} />
-          <Route path="/builders" element={<Builders />} />
-          <Route path="/roofers" element={<Roofers />} />
-          <Route path="/find-tradespeople" element={<FindTradespeople />} />
           <Route path="/trades-crm" element={<TradeCRMLayout />}>
             <Route index element={<Navigate to="dashboard" replace />} />
             <Route path="dashboard" element={<Dashboard />} />
@@ -64,9 +46,6 @@ const App = () => (
             <Route path="account-settings" element={<AccountSettings />} />
             <Route path="support" element={<TradeSupport />} />
           </Route>
-          <Route path="/reviews-reputation" element={<ReviewsReputation />} />
-          <Route path="/support" element={<Support />} />
-          <Route path="/trades" element={<Trades />} />
           <Route path="/trades/join" element={<TradeRegistration />} />
           <Route path="/join" element={<CustomerRegistration />} />
           <Route path="/login" element={<Login />} />
@@ -74,8 +53,6 @@ const App = () => (
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/verify-email" element={<VerifyEmail />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:id" element={<BlogPost />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
